d8f861ebf1dc68d5171c5e9573d3eb239fff5059
naim114 <naim.muhammad99@gmail.com>
2020-10-02 15:28:45 +0800

    mana boleh

---
+++ lib/pages/Homepage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 100,
+            title: Text('PDM Chat System'),
+            backgroundColor: Colors.amberAccent,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('from ADO DEN Haang'),
+            ),
+          ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return Container(
+                color: Colors.blue,
+                width: 200,
+                height: 150,
+              );
+            },
+            childCount: 4,
+          ))
+        ],
+      ),
+    );
+  }
+}
